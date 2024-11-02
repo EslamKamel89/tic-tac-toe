8d1bb2acd52cf78cb4ad09eac2e71a9f176ce541
EslamKamel89 <elsamkamelforex@gmail.com>
2024-11-02 05:52:59 +0200

    refactoring the state in the App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import Log from './Components/Log';
 import Player from "./Components/Player";
 
 function App() {
-  const [activePlayer, setActivePlayer] = useState('X');
+  // const [activePlayer, setActivePlayer] = useState('X');
   const [gameTurns, setGameTurns] = useState([]);
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((currentActivePlayer) => currentActivePlayer == 'X' ? 'O' : 'X');
+    // setActivePlayer((currentActivePlayer) => currentActivePlayer == 'X' ? 'O' : 'X');
     setGameTurns((prevTurns) => {
       let currentPlayer = 'X';
       if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
@@ -28,15 +28,22 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players" class="highlight-player">
-          <Player className={activePlayer == 'X' ? 'active' : ''} initialName="Player 1" symbol="X" />
-          <Player className={activePlayer == 'O' ? 'active' : ''} initialName="Player 2" symbol="O" />
+          <Player className={deriveActivePlayer(gameTurns) == 'X' ? 'active' : ''} initialName="Player 1" symbol="X" />
+          <Player className={deriveActivePlayer(gameTurns) == 'O' ? 'active' : ''} initialName="Player 2" symbol="O" />
         </ol>
-
         <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
       <Log gameTurns={gameTurns} />
     </main>
   )
+}
+
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = 'X';
+  if (gameTurns.length > 0 && gameTurns[0].player == 'X') {
+    currentPlayer = 'O';
+  }
+  return currentPlayer;
 }
 
 
--- src/Components/Log.jsx
+++ src/Components/Log.jsx
@@ -3,7 +3,11 @@ export default function Log({ gameTurns }) {
 
     return <ol id="log" >
         {gameTurns.map((value, index) => {
-            return <li className={index == 0 ? 'highlighted' : undefined}>{value.player + ' row: ' + value.square.row + ' col: ' + value.square.col}</li>
+            return <li
+                key={`Col:${value.square.col},Row:${value.square.row},index:${index}`}
+                className={index == 0 ? 'highlighted' : undefined}>
+                {`Player ${value.player} Selected{ ROW: ${value.square.row}, COL: ${value.square.col} }`}
+            </li>
         })}
     </ol>
 }
